1a4c669671597e00d54e4d8db9641a4ce85f1258
Nhat Huy <huyngnht1305@gmail.com>
2023-02-23 12:02:32 +0700

    add node modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 // import WorldMap from './pages/World-map/index';
-// import LineCharts  from './pages/Line-Chart/index';
+import LineCharts  from './pages/Line-Chart/index';
 import './index.css'
 
 export default function App() {
   return (
     <>
       {/* <WorldMap /> */}
-      {/* <LineCharts /> */}
-      <WorldMap />
+      <LineCharts />
     </>
   );
 };
