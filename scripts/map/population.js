9c0cafbdefe740adab7c3f664bbea194bdffe30f
Nhat Huy <huyngnht1305@gmail.com>
2023-05-09 14:27:37 +0700

    Chang population map

--- scripts/map/population.js
+++ scripts/map/population.js
@@ -1,5 +1,5 @@
 const width = 960,
-    height = 500;
+    height = 480;
 
 const years = [
     {year: 1990, value: "data1"}, {year: 1991, value: "data2"}, {year: 1992, value: "data3"}, {year: 1993, value: "data4"},
@@ -12,51 +12,17 @@ const years = [
 ]
 
 const options = [
-    {name: "Aitoff", projection: d3.geoAitoff()},
-    {name: "Albers", projection: d3.geoAlbers().scale(145).parallels([20, 50])},
-    {name: "August", projection: d3.geoAugust().scale(60)},
-    {name: "Baker", projection: d3.geoBaker().scale(100)},
-    {name: "Boggs", projection: d3.geoBoggs()},
-    {name: "Bonne", projection: d3.geoBonne().scale(120)},
-    {name: "Bromley", projection: d3.geoBromley()},
-    {name: "Collignon", projection: d3.geoCollignon().scale(93)},
-    {name: "Craster Parabolic", projection: d3.geoCraster()},
-    {name: "Eckert I", projection: d3.geoEckert1().scale(165)},
-    {name: "Eckert II", projection: d3.geoEckert2().scale(165)},
-    {name: "Eckert III", projection: d3.geoEckert3().scale(180)},
-    {name: "Eckert IV", projection: d3.geoEckert4().scale(180)},
-    {name: "Eckert V", projection: d3.geoEckert5().scale(170)},
-    {name: "Eckert VI", projection: d3.geoEckert6().scale(170)},
-    {name: "Eisenlohr", projection: d3.geoEisenlohr().scale(60)},
-    {name: "Equirectangular (Plate Carrée)", projection: d3.geoEquirectangular()},
-    {name: "Hammer", projection: d3.geoHammer().scale(165)},
-    {name: "Hill", projection: d3.geoHill()},
-    {name: "Goode Homolosine", projection: d3.geoHomolosine()},
-    {name: "Kavrayskiy VII", projection: d3.geoKavrayskiy7()},
-    {name: "Lambert cylindrical equal-area", projection: d3.geoCylindricalEqualArea()},
-    {name: "Lagrange", projection: d3.geoLagrange().scale(120)},
-    {name: "Larrivée", projection: d3.geoLarrivee().scale(95)},
-    {name: "Laskowski", projection: d3.geoLaskowski().scale(120)},
-    {name: "Loximuthal", projection: d3.geoLoximuthal()},
-    // {name: "Mercator", projection: d3.geoMercator().scale(490 / 2 / Math.PI)},
-    {name: "Miller", projection: d3.geoMiller().scale(100)},
-    {name: "McBryde–Thomas Flat-Polar Parabolic", projection: d3.geoMtFlatPolarParabolic()},
-    {name: "McBryde–Thomas Flat-Polar Quartic", projection: d3.geoMtFlatPolarQuartic()},
-    {name: "McBryde–Thomas Flat-Polar Sinusoidal", projection: d3.geoMtFlatPolarSinusoidal()},
-    {name: "Mollweide", projection: d3.geoMollweide().scale(165)},
-    {name: "Natural Earth", projection: d3.geoNaturalEarth()},
-    {name: "Nell–Hammer", projection: d3.geoNellHammer()},
-    {name: "Polyconic", projection: d3.geoPolyconic().scale(100)},
-    {name: "Robinson", projection: d3.geoRobinson()},
-    {name: "Sinusoidal", projection: d3.geoSinusoidal()},
-    {name: "Sinu-Mollweide", projection: d3.geoSinuMollweide()},
-    {name: "van der Grinten", projection: d3.geoVanDerGrinten().scale(75)},
-    {name: "van der Grinten IV", projection: d3.geoVanDerGrinten4().scale(120)},
-    {name: "Wagner IV", projection: d3.geoWagner4()},
-    {name: "Wagner VI", projection: d3.geoWagner6()},
-    {name: "Wagner VII", projection: d3.geoWagner7()},
-    {name: "Winkel Tripel", projection: d3.geoWinkel3()}
+    {name: "Equiangular (Plate Carrée)", projection: d3.geoEquirectangular()}
 ];
+
+d3.selectAll('.button-main').on('click', handleButtonClick);
+
+function handleButtonClick() {
+    d3.select(this)
+        .transition()
+        .duration(500) // set the duration of the transition in ms
+        // .style('background-color', 'red'); // set the new style for the button
+}
 
 options.forEach(function(o) {
     o.projection.rotate([0, 0]).center([0, 0]);
@@ -65,27 +31,29 @@ options.forEach(function(o) {
 let i = 0;
 let projection = options[i].projection;
 const path = d3.geoPath(projection);
-const graticule = d3.geoGraticule();
+// const graticule = d3.geoGraticule();
 const svg = d3.select("body")
     .append("svg")
+    .attr("class", "world")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("viewBox", `0 23 ${width} 455`);
 
 svg.append("defs")
     .append("path")
     .datum({type: "Sphere"})
     .attr("id", "sphere")
     .attr("d", path);
-svg.append("use")
-    .attr("class", "stroke")
-    .attr("xlink:href", "#sphere");
+// svg.append("use")
+//     .attr("class", "stroke")
+//     .attr("xlink:href", "#sphere");
 svg.append("use")
     .attr("class", "fill")
     .attr("xlink:href", "#sphere");
-svg.append("path")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("d", path);
+// svg.append("path")
+//     .datum(graticule)
+//     .attr("class", "graticule")
+//     .attr("d", path);
 
 const menu = d3.select("#projection-menu")
     .on("change", change)
