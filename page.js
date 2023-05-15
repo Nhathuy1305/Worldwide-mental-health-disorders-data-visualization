a878950550063303c06dc572a554f71b46e79904
Nhat Huy <huyngnht1305@gmail.com>
2023-05-15 09:57:14 +0700

    ZÃoom to Bounding bbox

--- page.js
+++ page.js
@@ -3,37 +3,39 @@ let activeButton = null;
 function redirectToPage(loggedIn) {
     const button = document.getElementById('button-' + loggedIn);
 
-    if (loggedIn === 1) {
-        if (activeButton != null && activeButton !== button) {
-            activeButton.classList.remove('active');
+    if (button) { // check if element exists
+        if (loggedIn === 1) {
+            if (activeButton != null && activeButton !== button) {
+                activeButton.classList.remove('active');
+            }
+            button.classList.toggle('active');
+            activeButton = button;
+            population();
         }
-        button.classList.toggle('active');
-        activeButton = button;
-        population();
-    }
-    else if (loggedIn === 2) {
-        if (activeButton != null && activeButton !== button) {
-            activeButton.classList.remove('active');
+        else if (loggedIn === 2) {
+            if (activeButton != null && activeButton !== button) {
+                activeButton.classList.remove('active');
+            }
+            button.classList.toggle('active');
+            activeButton = button;
+            age();
         }
-        button.classList.toggle('active');
-        activeButton = button;
-        age();
-    }
-    else if (loggedIn === 3) {
-        if (activeButton != null && activeButton !== button) {
-            activeButton.classList.remove('active');
+        else if (loggedIn === 3) {
+            if (activeButton != null && activeButton !== button) {
+                activeButton.classList.remove('active');
+            }
+            button.classList.toggle('active');
+            activeButton = button;
+            suicide();
         }
-        button.classList.toggle('active');
-        activeButton = button;
-        suicide();
-    }
-    else if (loggedIn === 4) {
-        if (activeButton != null && activeButton !== button) {
-            activeButton.classList.remove('active');
+        else if (loggedIn === 4) {
+            if (activeButton != null && activeButton !== button) {
+                activeButton.classList.remove('active');
+            }
+            button.classList.toggle('active');
+            activeButton = button;
+            disorder();
         }
-        button.classList.toggle('active');
-        activeButton = button;
-        disorder();
     }
 }
 
@@ -77,3 +79,64 @@ function changeToPreviousPage2(condition) {
         window.location.href = "not-found.html";
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const nextPageBtn = document.getElementById('page-change-btn');
+    if (nextPageBtn) { // check if element exists
+        nextPageBtn.addEventListener('click', function() {
+            const pageContainer = document.querySelector('.main-page');
+            pageContainer.classList.add('hidden');
+            setTimeout(function() {
+                window.location.href = 'vietnam.html';
+            }, 1500); // wait 0.5 seconds before navigating to next page
+        });
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const pageContainer = document.querySelector('.main-page');
+    if (pageContainer) {
+        pageContainer.classList.remove('hidden');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const nextPageBtn = document.getElementById('page-change-btn-2');
+    if (nextPageBtn) {
+        nextPageBtn.addEventListener('click', function() {
+            const pageContainer = document.querySelector('.main-page-2');
+            pageContainer.classList.add('hidden');
+            setTimeout(function() {
+                window.location.href = 'depress.html';
+            }, 1500); // wait 0.5 seconds before navigating to next page
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const pageContainer = document.querySelector('.main-page-2');
+    if (pageContainer) {
+        pageContainer.classList.remove('hidden');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const backPageBtn = document.getElementById('page-back-btn');
+    if (backPageBtn) {
+        backPageBtn.addEventListener('click', function() {
+            const pageContainer = document.querySelector('.back-main-page');
+            pageContainer.classList.add('hidden');
+            setTimeout(function() {
+                window.location.href = 'index.html';
+            }, 1500); // wait 0.5 seconds before navigating to next page
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const pageContainer = document.querySelector('.back-main-page');
+    if (pageContainer) {
+        pageContainer.classList.remove('hidden');
+    }
+});
